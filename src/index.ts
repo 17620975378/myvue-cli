aa0b44a36f03bc99e88bc5b1a50bf03f57f96735
lim <11113443+giteenimo@user.noreply.gitee.com>
2024-05-16 20:59:18 +0800

    feat: 修改package.json

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Command } from 'commander'
 import { version } from '../package.json'
 import create from './command/create'
 
-const program = new Command('myvue')
+const program = new Command('limvue')
 program.version(version, '-v, --version')
 
 program
--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -2,6 +2,7 @@ import { SimpleGit, simpleGit, SimpleGitOptions } from 'simple-git'
 import createLogger from 'progress-estimator'
 import chalk from 'chalk'
 import { log } from './log'
+import figlet from 'figlet'
 
 // 设置进度条
 const logger = createLogger({
@@ -19,6 +20,20 @@ const gitOptions: Partial<SimpleGitOptions> = {
   maxConcurrentProcesses: 6,
 }
 
+// const goodPrinter = async () => {
+//   const message = await figlet(
+//     '欢迎使用 limvue-cli 脚手架',
+//     function (err, data) {
+//       if (err) {
+//         log.error(String(err))
+//         return
+//       }
+//       console.log(data)
+//     }
+//   )
+//   console.log(chalk.rgb(40, 156, 193).visible(message))
+// }
+
 export const clone = async (
   pjName: string,
   url: string,
@@ -30,10 +45,11 @@ export const clone = async (
       estimate: 8000,
     })
     console.log()
-    console.log(chalk.blueBright('============================='))
-    console.log(chalk.blueBright('=====欢迎使用myvue脚手架====='))
-    console.log(chalk.blueBright('============================='))
+    console.log(chalk.blueBright('=================================='))
+    console.log(chalk.blueBright('=====欢迎使用limvue-cli脚手架====='))
+    console.log(chalk.blueBright('=================================='))
     console.log()
+    // goodPrinter()
     log.success(`项目创建成功: ${chalk.blueBright(pjName)}`)
     log.success('请执行以下命令启动项目:')
     log.info(` cd ${chalk.blueBright(pjName)}`)
